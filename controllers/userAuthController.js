9a55295e8dcde0035d95cabdd8314238e0101018
mazen <mazenmohamed181@gmail.com>
2023-08-06 23:30:59 +0300

    Handling errors in signup API usig try/catch

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -5,28 +5,32 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+  try {
+    const { error } = validate(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) {
-    return res.status(400).send("User already registered.");
-  }
-  user = new User(_.pick(req.body, ["email", "password"]));
-  const salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(user.password, salt);
-  user = await user.save();
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).send("User already registered.");
+    }
+    user = new User(_.pick(req.body, ["email", "password"]));
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(user.password, salt);
+    user = await user.save();
 
-  // By this we verify the user and sign in right away
-  // the user signed up.
-  // there is another method that user need to verify his mail first
-  // ten send the jwt.
-  const token = user.generateAuthToken();
-  res
-    .header("x-auth-token", token)
-    .send(_.pick(user, ["_id", "name", "email"]));
+    // By this we verify the user and sign in right away
+    // the moment the user signed up.
+    // there is another method that user need to verify his mail first
+    // then send the jwt.
+    const token = user.generateAuthToken();
+    res
+      .header("x-auth-token", token)
+      .send(_.pick(user, ["_id", "name", "email"]));
+  } catch (ex) {
+    res.status(500).send("Something failed.")
+  }
 };
 
 const signin = async (req, res) => {
