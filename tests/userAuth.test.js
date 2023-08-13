b9d243b3c3336c47b948952216b1674b74607bf4
mazen <mazenmohamed181@gmail.com>
2023-08-13 23:24:49 +0300

    Conducting unit/integration testing for signin API using (jest - supertest) packages

--- tests/userAuth.test.js
+++ tests/userAuth.test.js
@@ -56,3 +56,44 @@ describe("signup", () => {
     expect(res.statusCode).toBe(400);
   });
 });
+
+describe("signin", () => {
+  it("returns status code 200 and access token if valid email and password passed", async () => {
+    const res = await request(app).post("/signin").send(userData);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body["accessToken"]).toEqual(expect.any(String));
+  });
+
+  it("returns status code 400 if email isn't valid", async () => {
+    const res = await request(app)
+      .post("/signin")
+      .send({ email: "mazen", password: "123456789" });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("returns status code 400 if password isn't valid", async () => {
+    const res = await request(app)
+      .post("/signin")
+      .send({ email: "mazen@gmail.com", password: "123" });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("returns status code 400 if not existing email is passed", async () => {
+    const res = await request(app)
+      .post("/signin")
+      .send({ email: "mazen@gmail.com", password: "123456789" });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("returns status code 400 if wrong password for existing email is passed", async () => {
+    const res = await request(app)
+      .post("/signin")
+      .send({ email: "testest@gmail.com", password: "12345678" });
+
+    expect(res.statusCode).toBe(400);
+  });
+});
