e7bea25aa7a881a53b9f6125fa20b70e0535430f
mazen <mazenmohamed181@gmail.com>
2023-08-18 11:14:43 +0300

    add(Tests): editWallet integration/unit test

--- tests/wallet.test.js
+++ tests/wallet.test.js
@@ -8,11 +8,12 @@ const walletData = {
   currency: "EGP",
 };
 
+let newWallet = "Wakanda";
+
+let walletId;
+
 beforeAll(async () => {
-  await Wallet.findOneAndDelete({ name: walletData.name }).where({
-    userId: walletData.userId,
-  });
-  await Wallet.findOneAndDelete({ name: "testwallet2" }).where({
+  await Wallet.findOneAndDelete({ name: newWallet }).where({
     userId: walletData.userId,
   });
 });
@@ -20,6 +21,8 @@ beforeAll(async () => {
 describe("addWallet", () => {
   it("returns status code 200 if valid userId, name, currency passed", async () => {
     const res = await request(app).post("/addWallet").send(walletData);
+
+    walletId = res.body._id;
 
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual(expect.any(Object));
@@ -66,3 +69,37 @@ describe("addWallet", () => {
   });
 });
 
+describe("editWallet", () => {
+  it("returns status code 200 if valid userId, name, currency passed", async () => {
+    const res = await request(app).put(`/editWallet/${walletId}`).send({
+      userId: "64da97335d88662d7a9fc7c0",
+      name: newWallet,
+      currency: "EGP",
+      balance: 0,
+    });
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("returns status code 400 if valid existing wallet name passed", async () => {
+    const res = await request(app).put(`/editWallet/${walletId}`).send({
+      userId: "64ba7b4d9f55f2a82f86f64b",
+      name: newWallet,
+      currency: "EGP",
+      balance: 0,
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("returns status code 400 if not existing userId passed", async () => {
+    const res = await request(app).put(`/editWallet/${walletId}`).send({
+      userId: "64ba7b4d9f55f2a82f87f64b",
+      name: "newWallet",
+      currency: "EGP",
+      balance: 0,
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+});
