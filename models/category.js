37a73d1ace1b31db42f938b702d0f01918e3f1d9
mazen <mazenmohamed181@gmail.com>
2023-08-20 21:52:06 +0300

    add(Models): userId and enum validation to category

--- models/category.js
+++ models/category.js
@@ -2,9 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Joi = require("joi");
 
+const groupEnum = [
+  "Required Expense",
+  "Up & Comers",
+  "Fun & Relax",
+  "Investing & Debt Payments",
+  "Income",
+  "Other",
+];
+
+const typeEnum = ["Income", "Expense"];
+
 const Category = mongoose.model(
   "Category",
   new Schema({
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     name: {
       type: String,
       required: true,
@@ -13,19 +29,12 @@ const Category = mongoose.model(
     },
     group: {
       type: String,
-      enum: [
-        "Required Expense",
-        "Up & Comers",
-        "Fun & Relax",
-        "Investing & Debt Payments",
-        "Income",
-        "Other",
-      ],
+      enum: [...groupEnum],
       required: true,
     },
     type: {
       type: String,
-      enum: ["Income", "Outcome"],
+      enum: [...typeEnum],
       required: true,
     },
   })
@@ -33,9 +42,10 @@ const Category = mongoose.model(
 
 function validateCategory(category) {
   const schema = Joi.object({
+    userId: Joi.objectId().required(),
     name: Joi.string().min(2).max(50).required(),
-    group: Joi.string().required(),
-    type: Joi.string().required(),
+    group: Joi.any().valid(...groupEnum),
+    type: Joi.any().valid(...typeEnum),
   });
   return schema.validate(category);
 }
