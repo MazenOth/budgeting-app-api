81809e47013f0f08a033c254bab6a0fce35993f3
mazen <mazenmohamed181@gmail.com>
2023-08-25 10:18:11 +0300

    add(Routes): transactionRoutes

--- startup/routes.js
+++ startup/routes.js
@@ -2,6 +2,7 @@ const bodyParser = require("body-parser");
 const userAuthRoutes = require("../routes/userAuthRoutes");
 const walletRoutes = require("../routes/walletRoutes");
 const categoryRoutes = require("../routes/categoryRoutes");
+const transactionRoutes = require("../routes/transactionRoutes");
 const home = require("../routes/home");
 const error = require("../middleware/error");
 
@@ -11,6 +12,7 @@ module.exports = function (app) {
   app.use(userAuthRoutes);
   app.use(walletRoutes);
   app.use(categoryRoutes);
+  app.use(transactionRoutes);
   app.use(home);
   app.use(error);
 };
