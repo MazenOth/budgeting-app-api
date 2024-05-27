b0fd05c6fb1f84010d26dffa83851eacd8a1fa80
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2024-05-27 11:39:53 +0300

    feat(addRecurring): adding Create endpoint for Recurring Transactions
    - Creating an route handler to adding new recurring transactions

--- controllers/recurringTransaction.js
+++ controllers/recurringTransaction.js
@@ -1,10 +1,11 @@
-const { Transaction, validate } = require("../models/transaction");
-const { Wallet } = require("../models/wallet");
-const { User } = require("../models/user");
-const { Category } = require("../models/category");
-const mongoose = require("mongoose");
-const _ = require("lodash");
-const schedule = require("node-schedule");
+const {
+  RecurringTransaction,
+  validate,
+} = require('../models/recurringTransaction');
+const { Wallet } = require('../models/wallet');
+const { User } = require('../models/user');
+const { Category } = require('../models/category');
+const _ = require('lodash');
 
 const addRecurring = async (req, res) => {
   const { error } = validate(req.body);
@@ -14,74 +15,36 @@ const addRecurring = async (req, res) => {
 
   const wallet = await Wallet.findById(req.params.walletId);
   if (!wallet) {
-    return res.status(400).send("Please check your walletId.");
+    return res.status(400).send('Please check your walletId.');
   }
   const user = await User.findById(req.params.userId);
   if (!user) {
-    return res.status(400).send("Please check your userId.");
+    return res.status(400).send('Please check your userId.');
   }
-  const category = await Category.findOne({
-    name: req.body.categoryName,
-  }).where({
+  const category = await Category.findById(req.body.categoryId).where({
     walletId: req.params.walletId,
   });
   if (!category) {
-    return res.status(400).send("Please check your category name.");
+    return res.status(400).send('Please check your category id.');
   }
 
-  let transaction = new Transaction({
+  let recurringTransaction = new RecurringTransaction({
     wallet: {
       _id: wallet._id,
-      name: wallet.name,
     },
     user: {
       _id: user._id,
-      name: user.name,
     },
     category: {
       _id: category._id,
-      name: category.name,
-      group: category.group,
-      type: category.type,
     },
     amount: req.body.amount,
-    transactionDate: req.body.transactionDate,
-    recurring: true,
     startDate: req.body.startDate,
-    endDate: req.body.endDate,
-    frequency: req.body.frequency,
     frequencyType: req.body.frequencyType,
+    nextOccurence: req.body.startDate,
   });
-
-  const job = schedule.scheduleJob(
-    {
-      start: transaction.startDate,
-      end: transaction.endDate,
-      rule: "*/1 * * * * *",
-    },
-    async function () {
-      try {
-        const session = await mongoose.startSession();
-        await session.withTransaction(async () => {
-          const result = await transaction.save();
-          if (transaction.category.type === "Expense") {
-            var newBalance = wallet.balance - transaction.amount;
-            wallet.balance = newBalance;
-          } else {
-            var newBalance = wallet.balance + transaction.amount;
-            wallet.balance = newBalance;
-          }
-          wallet.save();
-          res.send(result);
-        });
-
-        session.endSession();
-        console.log("Transaction succeeded.");
-      } catch (error) {
-        console.log("transaction error: ", error.message);
-      }
-    }
-  );
+  recurringTransaction = await recurringTransaction.save();
+  res.send(recurringTransaction);
 };
 
 module.exports = {
